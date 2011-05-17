beeaeea17bbae28ded9c6d507863d07c0b83ef9f
Andre Silva <macolinkalkin@gmail.com>
2011-05-17 00:44:37 -0300

    Incluindo Componente

--- src/main/java/br/com/gonow/projeto/dao/EntidadeDAO.java
+++ src/main/java/br/com/gonow/projeto/dao/EntidadeDAO.java
@@ -1,7 +1,9 @@
 package br.com.gonow.projeto.dao;
 
+import br.com.caelum.vraptor.ioc.Component;
 import br.com.gonow.projeto.dao.repository.EntidadeDAORepository;
 
+@Component
 public class EntidadeDAO implements EntidadeDAORepository {
 
 }
